ad44851a4a42db3a26c2e342481c65242e0b0155
Jon Fairbanks <fairplay89@gmail.com>
2023-05-12 00:27:07 -0700

    v1.0.0

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Build stage
+FROM python:3.9-alpine3.14 as builder
+
+RUN apk update && apk add python3-dev gcc libc-dev
+RUN pip install --upgrade pip
+RUN pip install pipenv streamlit
+
+WORKDIR /app
+COPY ./src/Pipfile* ./
+RUN pipenv lock && pipenv requirements > requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt --prefix=/install
+
+# Final stage
+FROM python:3.9-alpine3.14
+
+WORKDIR /app
+
+# Copy only the installed packages from the build stage
+COPY --from=builder /install /usr/local
+
+COPY --chown=nobody:nogroup ./src .
+
+USER nobody
+
+EXPOSE 8080
+
+CMD ["streamlit", "run", "index.py"]
--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import archiver
+import datetime
 import re
 import streamlit as st
 
@@ -6,8 +7,18 @@ import streamlit as st
 st.set_page_config(
     page_title="Thingiverse Collection Archiver",
     layout="wide",
-    initial_sidebar_state="auto"
+    initial_sidebar_state="auto",
 )
+
+# Hide anchor links on titles
+hide_anchor_link = """
+        <style>
+        .css-15zrgzn {display: none}
+        .css-eczf16 {display: none}
+        .css-jn99sy {display: none}
+        </style>
+        """
+st.markdown(hide_anchor_link, unsafe_allow_html=True)
 
 # Hide the Streamlit footer
 hide_streamlit_style = """
@@ -16,32 +27,46 @@ hide_streamlit_style = """
     footer {visibility: hidden;}
     </style>
     """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
 def main():
-    st.title("Thingiverse Collection Archiver") # TODO: Figure out how to disable the link option
-    st.write("Provide a Thingiverse collection and we’ll extract all of the items into an easily downloadable .zip archive")
+    st.title("Thingiverse Collection Archiver")
+    st.write(
+        "Provide a Thingiverse collection and we’ll extract all of the items into an easily downloadable .zip archive"
+    )
     with st.form("url_form"):
-        url = st.text_input("Enter a URL:", placeholder="https://www.thingiverse.com/<username>/collections/<collection_id>/things")
+        url = st.text_input(
+            "Enter a URL:",
+            placeholder="https://www.thingiverse.com/<username>/collections/<collection_id>/things",
+        )
         submit_button = st.form_submit_button("Archive")
         zip_path = None  # Default value for zip_path
         if submit_button and url:
-            url_pattern = re.compile(r'^https://www.thingiverse.com/\w+/collections/\w+/things$')
+            url_pattern = re.compile(
+                r"^https://www.thingiverse.com/\w+/collections/\w+/things$"
+            )
             if url_pattern.match(url):
                 collection_id = re.search(r"/collections/(\d+)/", url).group(1)
-                with st.spinner("Generating archive..."): # TODO: If the archive is generating, hide the archive button
-                    file_path = archiver.download_collection(url) # TODO: If the file exists, do not download it
+                with st.spinner(
+                    "Generating archive..."
+                ):  # TODO: If the archive is generating, hide the archive button
+                    file_path = archiver.download_collection(url)
                     zip_path = archiver.archive_collection(collection_id)
             else:
-                st.write("Please enter a valid Thingiverse collection URL") # TODO: Can this text be red?
+                st.write(
+                    '<span style="color: red;">Please enter a valid Thingiverse collection URL</span>',
+                    unsafe_allow_html=True,
+                )
     if zip_path is not None:
         with open(zip_path, "rb") as fp:
             btn = st.download_button(
                 label="Save Archive",
                 data=fp,
                 file_name=f"{zip_path}",
-                mime="application/zip"
+                mime="application/zip",
             )
+
 
 # Add a footer
 def footer():
@@ -56,8 +81,13 @@ def footer():
     st.markdown(footer_style, unsafe_allow_html=True)
 
     st.markdown("---")
-    st.markdown("<div class='footer'>This tool is not affiliated with Ultimaker and/or Thingiverse</div>", unsafe_allow_html=True)
-    st.markdown("<div class='footer'>Copyright © 2023</div>", unsafe_allow_html=True) # TODO: Do not hardcode the year
+    st.markdown(
+        "<div class='footer'>This tool is not affiliated with Ultimaker and/or Thingiverse</div>",
+        unsafe_allow_html=True,
+    )
+    copyright = f"<div class='footer'>Copyright © {datetime.datetime.now().year}</div>"
+    st.markdown(copyright, unsafe_allow_html=True)
+
 
 if __name__ == "__main__":
     main()
